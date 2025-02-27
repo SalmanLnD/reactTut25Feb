dd0d2c555c11c2888e4a84c7977e7445a6b6911c
Muhammed Salman <salman8official@gmail.com>
2025-02-27 15:48:56 +0530

    Update Navbar.jsx

--- Feb25Tut/src/component/Navbar/Navbar.jsx
+++ Feb25Tut/src/component/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/dashboard">Dashboard</Link></li>
+      </ul>
+    </nav>
   )
 }
 
